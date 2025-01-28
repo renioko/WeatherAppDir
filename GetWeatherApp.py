f93318cae5e89b896eb91a5bfb772fdba31ca40e
Renata Gasior <renatta.gasior@gmail.com>
2025-01-28 14:27:23 +0000

    removed unused imports, removed unnecessary notes

--- GetWeatherApp.py
+++ GetWeatherApp.py
@@ -1,13 +1,10 @@
 
 from load_weather_api_key import load_weather_api_key
 from load_weather_api_params import weather_api_params
-import flask
 import json
 import requests
 import sys
-import tomllib
 
-from requests import status_codes
 from requests import HTTPError
 
 '''
@@ -43,7 +40,7 @@ def get_request(location) -> json:
 def print_raport(weather_json):
     place = weather_json['location']
     weather = weather_json['current']
-    print(f'Today the weather in {place['name']}, {place['region']}, {place['country']}) at the time: {place['localtime']} is as follows:')
+    print(f'Today the weather in {place['name']}, {place['region']}, {place['country']} at the time: {place['localtime']} is as follows:')
     print(f'temperature: {weather['temp_c']}C,')
     print(f'feels-like temp: {weather['feelslike_c']}C')
     print(f'wind: {weather['wind_kph']}kph, wind direction: {weather['wind_dir']},')
@@ -59,7 +56,6 @@ def main():
 if __name__ == '__main__':
     main()
 
-{"location":{"name":"Wednesbury","region":"West Midlands","country":"UK","lat":52.560001373291,"lon":-2.02099990844727,"tz_id":"Europe/London","localtime_epoch":1732645892,"localtime":"2024-11-26 18:31"},
- "current":{"last_updated":"2024-11-26 18:30","temp_c":4.3,"is_day":0,"condition":{"text":"Clear","icon":"//cdn.weatherapi.com/weather/64x64/night/113.png","code":1000},"wind_mph":2.9,"wind_kph":4.7,"wind_degree":181,"wind_dir":"S","pressure_mb":1017.0,"pressure_in":30.03,"humidity":93,"cloud":0,"feelslike_c":3.4,"windchill_c":3.9,"windchill_f":39.1,"gust_mph":5.4,"gust_kph":8.7}}
+
 
 
